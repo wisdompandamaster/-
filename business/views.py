6d15ad26e946e738d9786ca166dd2eb6d97b1d6e
wisdompandamaster <2532569059@qq.com>
2020-09-09 21:14:15 +0800

    complete some I/F

--- business/views.py
+++ business/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Tag, Task, Done, User
 from django.db.models import Q
-
+from django.utils import timezone
 
 # Create your views here.
 
@@ -89,6 +89,7 @@ def ShowList(request):
             doneFileName = done.doneFileName
             zanList = list(map(int, done.zanList.split(",")))
             print(zanList)
+
             data = {"id": str(ID), "Tag": {"content": content, "color": color},
                     "startTime": startTime, "doneTime": doneTime,
                     "startDep": startDep, "receiveDep": receiveDep, "message": message,
@@ -100,19 +101,44 @@ def ShowList(request):
 
 
 def Accept(request):
-    return None
+    if request.method == "POST":
+        ID = int(request.POST.get('id'))
+        print(ID)
+        task = Task.objects.filter(id=ID)
+        print(task)
+        task.update(status=1, acceptTime=timezone.now())
+    return JsonResponse({"status": task[0].status})
 
 
 def WithDraw(request):
-    return None
+    if request.method == "POST":
+        ID = int(request.POST.get('id'))
+        Task.objects.filter(id=ID).delete()
+        Done.objects.filter(task__id=ID).delete()
+        withdraw = {"msg": "成功撤销id="+str(ID)+"号事务"}
+    return JsonResponse(withdraw)
 
 
 def Submit(request):
     return None
 
 
-def Zan(request):
-    return None
+def Zan(request):      # 先把zanList转化为int列表，检测是否部门id在其中，在就删除，不在就添加，再转为字符串
+    if request.method == "POST":
+        ID = int(request.POST.get('id'))
+        department = request.POST.get('department')
+        zan = Done.objects.filter(task__id=ID)[0]
+        Zan = zan.zanList.split(',')
+        print(Zan)
+        if department in Zan:            # 赞过就取消，没赞过就点赞
+            Zan.remove(department)
+        else:
+            Zan.append(department)
+        zanList = ','.join(Zan)
+        zan.zanList = zanList
+        zan.save()
+       # Zan.update(zanList=Zan[0].zanList + "," + str(department))
+    return JsonResponse({"msg": "点赞成功"})
 
 
 def Start(request):
